07d958cf3fd83d1118debdb33ff06f1a73241926
Esteven <ehsteven@gmail.com>
2025-07-18 01:30:12 -0600

    `Update z-index values to 5000 in various components`

--- src/components/records/DateFieldWithNotifications.jsx
+++ src/components/records/DateFieldWithNotifications.jsx
@@ -165,7 +165,7 @@ const DateFieldWithNotifications = ({
 
       {showNotificationModal && (
         <div
-          className="fixed inset-0 bg-black/50 flex items-center justify-center z-[60]"
+          className="fixed inset-0 bg-black/50 flex items-center justify-center z-[5000]"
           onClick={(e) => {
             if (e.target === e.currentTarget) {
               setShowNotificationModal(false);
