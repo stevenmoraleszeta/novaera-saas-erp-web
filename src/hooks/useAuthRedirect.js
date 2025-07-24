2aaa52bd03ef91584652950eb1b1e716a33f647f
Dylanbt2227 <dylantorres2227@gmail.com>
2025-07-24 12:09:59 -0600

    Update useAuthRedirect.js

--- src/hooks/useAuthRedirect.js
+++ src/hooks/useAuthRedirect.js
@@ -1,16 +1,1 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
 
-export const useAuthRedirect = () => {
-  const router = useRouter();
-
-  useEffect(() => {
-    const hasToken = document.cookie
-      .split(';')
-      .some(cookie => cookie.trim().startsWith('token='));
-
-    if (!hasToken) {
-      router.replace('/');
-    }
-  }, [router]);
-};
